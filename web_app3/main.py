e782e1bd242bdf6e79083f2869d5da62544e0201
mayukh656 <srinivasmayukh.web@gmail.com>
2023-01-16 17:20:19 -0600

    Initial commit - pdf

---
+++ web_app3/main.py
@@ -0,0 +1,16 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit='mm', format="A4")
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_text_color(0, 0, 254)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 20, 200, 20)
+
+
+pdf.output("output.pdf")
